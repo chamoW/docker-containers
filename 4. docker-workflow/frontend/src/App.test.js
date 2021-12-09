afa50a61c48d2b184dc1141bbbe9dbbbbbabaa38
Wilson Wladimir Lopez Siguencia <wlopez@Todo1of.com.ec>
2021-12-09 10:37:19 -0500

    Configuracipón de un contenedor para un proyecto con reactjs

---
+++ 4. docker-workflow/frontend/src/App.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/DOCKER/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('Nueva prueba', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/DOCKER/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('Nueva prueba duplicada', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/DOCKER/i);
+  expect(linkElement).toBeInTheDocument();
+});
+ 
+
+test('Nueva prueba duplicada FALLA', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/FALLA/i);
+  expect(linkElement).toBeInTheDocument();
+});
