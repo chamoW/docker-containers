3a48417fc0cf28fdf3d9c42344b05868419b6530
Wladimir Lopez <wllopez@technisys.com>
2022-10-06 23:18:58 -0500

    Arrange tests

--- 4. docker-workflow/frontend/src/App.test.js
+++ 4. docker-workflow/frontend/src/App.test.js
@@ -3,27 +3,27 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/DOCKER/i);
+  const linkElement = screen.getByText(/PROBANDO/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 
 test('Nueva prueba', () => {
   render(<App />);
-  const linkElement = screen.getByText(/DOCKER/i);
+  const linkElement = screen.getByText(/amazing/i);
   expect(linkElement).toBeInTheDocument();
 });
-
 
 test('Nueva prueba duplicada', () => {
   render(<App />);
-  const linkElement = screen.getByText(/DOCKER/i);
+  const linkElement = screen.getByText(/PROBANDO/i);
   expect(linkElement).toBeInTheDocument();
 });
  
-
+/*
 test('Nueva prueba duplicada FALLA', () => {
   render(<App />);
   const linkElement = screen.getByText(/FALLA/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
